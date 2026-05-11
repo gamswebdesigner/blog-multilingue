bbf4843dc94fa155cc43121466f2cab80e0d7b69
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-11 00:06:28 -0300

    Add relationships and name accessor to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoryFactory> */
-    use HasFactory;
+    protected $fillable = ['slug', 'name_de', 'name_en'];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function getNameAttribute(): string
+    {
+        return $this->{'name_' . app()->getLocale()};
+    }
 }
