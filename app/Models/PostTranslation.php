b65bef9292e0af94a32f617e2132cf49d97c5be7
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-11 00:36:22 -0300

    Add relationships to PostTranslation and User models

--- app/Models/PostTranslation.php
+++ app/Models/PostTranslation.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PostTranslation extends Model
 {
-    //
+    protected $fillable = ['post_id', 'locale', 'title', 'body'];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
