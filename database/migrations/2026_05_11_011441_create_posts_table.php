1980314a9c6f60879a18715f2b9ab2a584d07eb0
Guilherme De Araújo Martins Sabino <gamswebdesigner@gmail.com>
2026-05-10 22:37:26 -0300

    Add schema to posts migration with foreign keys

--- database/migrations/2026_05_11_011441_create_posts_table.php
+++ database/migrations/2026_05_11_011441_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('slug')->unique();
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
